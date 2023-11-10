13921abce91e36ffbeedf6e8aa17968228bf72a9
Pinky <rnspinky@gmail.com>
2023-11-10 21:45:04 +0530

    TODO List

--- Index.js
+++ Index.js
@@ -1,90 +1,32 @@
-let RandomNumber = Math.floor((Math.random() * 99) + 1);
-const GuessField = document.querySelector("#guessField");
-const Submit = document.querySelector("#submit");
-const Guesses = document.querySelector(".guesses");
-const lastResult = document.querySelector(".lastResult");
-const lowOrHi = document.querySelector(".lowOrHi");
-const resultPress = document.querySelector(".resultPress");
+const Form = document.querySelector('form')
+    const writeText = document.querySelector("form input[type='text']");
+    // const Add = document.querySelector(".submit");
+    const Done = document.querySelector(".done");
+    const Remove = document.querySelector(".remove");
+    const Ull = document.querySelector(".ul-todo")
 
-
-const Para = document.createElement('p');
-
-let prevGuess = [];
-let numGuess = 1;
-let playgame = true;
-if (playgame) {
-    Submit.addEventListener('click', function (e) {
+    Form.addEventListener('submit', function (e) {
         e.preventDefault();
-        const Guess = parseInt(GuessField.value);
-        validateGuess(Guess);
+        const Value = writeText.value;
+        const newList = document.createElement('li');
+        const newListDetails = `<span class="text">${Value}</span>
+                <div class="todo-btn">
+                    <button class="button done">Done</button>
+                    <button class="button remove">Remove</button>
+                </div>`;
+        newList.innerHTML = newListDetails;
+        Ull.append(newList);
+        writeText.value = "";
 
     });
-}
-function validateGuess(Guess) {
-    if (isNaN(Guess)) {
-        alert("Please enter a valid number");
-    }
-    else if (Guess < 1) {
-        alert("Please enter a number more than 1");
-    }
-    else if (Guess > 100) {
-        alert("Please enter a number less than 100");
-    }
-    else {
-        prevGuess.push(Guess);
-        if (numGuess === 11) {
-            displayMessage(`Game Over .Random number was ${RandomNumber} `);
-            endGame();
+
+    Ull.addEventListener('click', function (e) {
+        if (e.target.classList.contains("done")) {
+            const Lispan = e.target.parentNode.previousElementSibling;
+            Lispan.style.textDecoration = "line-through";
         }
-        else {
-            displayGuess(Guess);
-            checkGuess(Guess)
+        if (e.target.classList.contains("remove")) {
+            const Targetedli = e.target.parentNode.parentNode;
+            Targetedli.remove();
         }
-    }
-}
-
-function checkGuess(Guess) {
-    if (Guess === RandomNumber) {
-        displayMessage(`You guessed it right`);
-        endGame()
-    } else if (Guess < RandomNumber) {
-        displayMessage(`Number is TOOO low`)
-    }
-    else if (Guess > RandomNumber) {
-        displayMessage(`Number is TOOO High`)
-    }
-}
-function displayGuess(Guess) {
-    GuessField.value = '';
-    Guesses.innerHTML += `${Guess},  `;
-    numGuess++;
-    lastResult.innerHTML = `${11 - numGuess}`;
-}
-
-function displayMessage(message) {
-    lowOrHi.innerHTML = `<p>${message}</p>`;
-}
-
-function endGame() {
-    GuessField.value = '';
-    GuessField.setAttribute('disabled', '');
-    Para.classList.add('button');
-    Para.innerHTML = `<h2 id="newGame">Start new Game</h2>`;
-    resultPress.append(Para);
-    playgame = false;
-    newGame();
-}
-function newGame() {
-    const newGameButton = document.querySelector('#newGame');
-    newGameButton.addEventListener('click', function (e) {
-        RandomNumber = Math.floor((Math.random() * 99) + 1);
-        prevGuess = [];
-        numGuess = 1;
-        Guesses.innerHTML = '';
-        lowOrHi.innerHTML = '';
-        lastResult.innerHTML = `${11 - numGuess}`;
-        GuessField.removeAttribute('disabled');
-        resultPress.removeChild(Para);
-        playgame = true;
     })
-}
